5f8bd0117a332899d3b1d8c17512be35f9675a06
Clara Mayorga <mayorgaclara@gmail.com>
2021-08-08 00:41:10 -0300

    User and Product latest

--- src/controller/api/apiUserController.js
+++ src/controller/api/apiUserController.js
@@ -1,9 +1,11 @@
 const list = require('./apiUserController/list');
 const detail = require('./apiUserController/detail');
+const latest = require('./apiUserController/latest');
 
 const controller = {
     list,
     detail,
+    latest,
 };
 
 module.exports = controller;
